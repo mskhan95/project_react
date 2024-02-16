ad884b8b1fd27dfd48a63f6aa276b1ad394e5fbd
mskhan1995 <shabhankhan123456@gmail.com>
2024-02-16 20:00:46 +0530

    chakra ui Added

--- src/Components/Navbar_Page/Navbar.jsx
+++ src/Components/Navbar_Page/Navbar.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import logo from "../../assets/Navbar/logo.png";
 import "./Navbar.css";
+import { Center } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
     <div className="navbar_maincontainer">
-      <div className="second_container">
-        <div className="logo_Address_container">
-          <div className="logo_container">
-            <img className="logo_img" src={logo} alt="Logo" />
+      <Center>
+        <div style={{ maxWidth: "1200px" }}>
+          <div className="second_container">
+            <div className="logo_Address_container">
+              <div className="logo_container">
+                <img className="logo_img" src={logo} alt="Logo" />
+              </div>
+              <div className="navbar_Address">Address</div>
+            </div>
+            <div className="item_list_container">
+              <ul className="navbar_list">
+                <li>Search</li>
+                <li>Offers</li>
+                <li>Help</li>
+                <li>SignIn</li>
+                <li>Cart</li>
+              </ul>
+            </div>
           </div>
-          <div className="navbar_Address">Address</div>
         </div>
-        <div className="item_list_container">
-          <ul className="navbar_list">
-            <li>Search</li>
-            <li>Offers</li>
-            <li>Help</li>
-            <li>SignIn</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
+      </Center>
     </div>
   );
 };
